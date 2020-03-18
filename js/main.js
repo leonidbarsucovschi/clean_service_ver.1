4c5f2565330d43dc243db97df67cffd015b7d177
Leonid Barsucovschi <leonidbarsucovschi0503@gmail>
2020-03-18 08:28:24 +0200

    clean_service_ver.1

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(function(){
+
+  $('.slider__inner, .prise__slider-inner').slick({
+    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+    infinite: false
+  });
+
+  $('select').styler();
+
+  $('.header__btn-menu').on('click', function(){
+    $('.menu ul').slideToggle();
+  });
+
+});
+
+$(document).ready(function(){
+    $(".menu,.header__contacts").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 800);
+    });
+});
+
